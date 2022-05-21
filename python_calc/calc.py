17220f47165e2dd26edc155734d0fa702c096742
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-21 10:51:11 +0100

    simple calculator

---
+++ python_calc/calc.py
@@ -0,0 +1,20 @@
+# Calculator designed in python code
+
+numb1 = int(input('Enter your first number'))
+numb2 = int(input('Enter your second number'))
+
+# Addition
+
+print('{} + {} ='.format(numb1, numb2), numb1 + numb2)
+
+# Subtraction
+
+print('{} + {} ='.format(numb1, numb2), numb1 - numb2)
+
+# Division
+
+print('{} + {} ='.format(numb1, numb2), numb1 / numb2)
+
+# Multiplication
+
+print('{} + {} ='.format(numb1, numb2), numb1 * numb2)
