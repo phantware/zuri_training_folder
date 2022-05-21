c1a5deac832712bd3859e751abc7c6dac9ee94bd
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-21 11:36:51 +0100

    advance calc

---
+++ python_calc/advanceCalc.py
@@ -0,0 +1,16 @@
+# Advance Calculator
+class Calculator:
+
+ # This is a stact-based calculator
+ # Uses parenthesis for order of Operation
+ # Implemets addition, subtraction, division and multiplication
+
+   def calculate(self, s):
+    # Takes in a string s and returns a number
+      return
+print("Launching the calculator...")
+calc = Calculator()
+user_input = ""
+while user_input not in ["exit","quite","stop", "close"]:
+ user_input = input("> ")
+ output = calc.calculate(user_input)
