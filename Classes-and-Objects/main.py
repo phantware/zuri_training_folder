c5d2ac485ef1a5a7b0aa804db7b292a3d2502a2b
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-26 10:27:45 +0100

    class and object in python

--- Classes-and-Objects/main.py
+++ Classes-and-Objects/main.py
@@ -1,15 +1,20 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-  
-    def __init__(self):
-        pass
-
-
+    pass
+    def __init__(self, name, age, tracks,score):
+        def change_name(self, name):
+            self.name = name
+        self.change_name = name
+        self.hange_age = age
+        self.dd_track = tracks
+        self.get_score = score
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
-# Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
+Bob.change_name("Peter")   
+# Bob.change_age = 34
+# Bob.add_track = "UI/UX"
+# Bob.get_score
+
+# print(Bob.change_name,Bob.change_age,Bob.add_track,Bob.get_score)
+print(Bob.change_name)
