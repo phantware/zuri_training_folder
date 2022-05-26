c51b85bafeff239bf1d4a36f5af2e00e7b81f664
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-26 11:30:09 +0100

    Zuri class and object in python

--- Classes-and-Objects/main.py
+++ Classes-and-Objects/main.py
@@ -2,19 +2,24 @@ class Student:
     # [assignment] Skeleton class. Add your code here
     pass
     def __init__(self, name, age, tracks,score):
-        def change_name(self, name):
-            self.name = name
-        self.change_name = name
-        self.hange_age = age
-        self.dd_track = tracks
-        self.get_score = score
+        self.name = name
+        self.age = int(age)
+        self.tracks = tracks
+        self.score = float(score)
 
+    def change_name(self, name):
+            self.name = name
+    def change_age(self, age):
+            self.age = age
+    def add_track(self, tracks):
+            self.tracks = tracks
+    def get_score(self):
+            return self.score
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 Bob.change_name("Peter")   
-# Bob.change_age = 34
-# Bob.add_track = "UI/UX"
-# Bob.get_score
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
 
-# print(Bob.change_name,Bob.change_age,Bob.add_track,Bob.get_score)
-print(Bob.change_name)
+print(Bob.name,Bob.age,Bob.tracks,Bob.score)
