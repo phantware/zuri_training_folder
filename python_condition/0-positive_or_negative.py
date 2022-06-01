5507bec5be7b0de4f74e5517289409b69f6e3199
phantware <phantmoney2011@gmail.com>
2022-06-01 21:35:53 +0100

    python conditions added

---
+++ python_condition/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+if number > 0:
+   number = 0
+   print("Number is negative")
+
+elif number == 1:
+     print("Number is Positive")
+else:
+   print("number is either positive or negative")
+  
