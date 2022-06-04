b251e1d33a6b7674e2846d63d150fa1a4dfe2e62
phantware <phantmoney2011@gmail.com>
2022-06-04 16:08:36 +0100

    check user input against computer

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,4 +1,5 @@
 import random
+from traceback import print_tb
 
 user_choice = input("Rock, Paper, or Scissors: ")
 
@@ -11,5 +12,30 @@ elif random_num == 2:
  cpu_choice = "scissors"
 
 print()
-print("Your choice:", user_choice)
-print("Computer's choice:", cpu_choice)
+print("Your choice: ", user_choice)
+print("Computer's choice: ", cpu_choice)
+print()
+
+if user_choice == "rock":
+ if cpu_choice == "rock":
+  print("It's a tie!")
+ elif cpu_choice == "paper":
+  print("You lost!")
+ elif cpu_choice == "scissors":
+  print("You win!")
+
+elif user_choice == "paper":
+ if cpu_choice == "paper":
+  print("It's a tie!")
+ elif cpu_choice == "scissors":
+  print("You lost!")
+ elif cpu_choice == "rock":
+  print("You win!")
+
+elif user_choice == "scissors":
+ if cpu_choice == "scissors":
+  print("It's a tie!")
+ elif cpu_choice == "rock":
+  print("You lost!")
+ elif cpu_choice == "paper":
+  print("You win!")
