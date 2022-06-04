91e99f65cee249a092f45d998618930dc86045ab
phantware <phantmoney2011@gmail.com>
2022-06-04 15:01:00 +0100

    conditions added to file one

--- modules/file_one.py
+++ modules/file_one.py
@@ -2,7 +2,19 @@ import file_two
 
 print("File one __name__ is set to: {}" .format(__name__))
 
+def function_one():
+ print("Function one is executed")
+
+def function_two():
+ print("Function two is executed")
+
+# if __name__ == "__main__":
+#  print("File one executed when ran directly")
+# else:
+#  print("File one executed when imported")
+
 if __name__ == "__main__":
- print("File one executed when ran directly")
+   print("File one executed when ran directly")
+   function_two()
 else:
- print("File one executed when imported")
+   print("File one executed when imported")
