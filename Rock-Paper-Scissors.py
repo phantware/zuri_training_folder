16e1d3bf4d721f0ed5fc07d57268419567216c8e
phantware <phantmoney2011@gmail.com>
2022-06-04 16:34:17 +0100

    reiteration added using while True

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,43 +1,47 @@
 import random
 
-user_choice = input("Rock, Paper, or Scissors: ").lower()
+input("Welcome to Rock, Paper and Scissors! Press enter to start ")
 
-while user_choice != "rock" and user_choice != "paper" and user_choice != "scissors":
- user_choice = input("Invalid input, please try again: ").lower()
+while True:
+ print()
+ user_choice = input("Rock, Paper, or Scissors?: ").lower()
 
-random_num = random.randint(0,2)
-if random_num == 0:
- cpu_choice = "rock"
-elif random_num == 1:
- cpu_choice = "paper"
-elif random_num == 2:
- cpu_choice = "scissors"
+ while user_choice != "rock" and user_choice != "paper" and user_choice != "scissors":
+  user_choice = input("Invalid input, please try again: ").lower()
 
-print()
-print("Your choice: ", user_choice)
-print("Computer's choice: ", cpu_choice)
-print()
+ random_num = random.randint(0,2)
+ if random_num == 0:
+  cpu_choice = "rock"
+ elif random_num == 1:
+  cpu_choice = "paper"
+ elif random_num == 2:
+  cpu_choice = "scissors"
 
-if user_choice == "rock":
- if cpu_choice == "rock":
-  print("It's a tie!")
- elif cpu_choice == "paper":
-  print("You lost!")
- elif cpu_choice == "scissors":
-  print("You win!")
+ print()
+ print("Your choice: ", user_choice)
+ print("Computer's choice: ", cpu_choice)
+ print()
 
-elif user_choice == "paper":
- if cpu_choice == "paper":
-  print("It's a tie!")
- elif cpu_choice == "scissors":
-  print("You lost!")
- elif cpu_choice == "rock":
-  print("You win!")
+ if user_choice == "rock":
+  if cpu_choice == "rock":
+   print("It's a tie!")
+  elif cpu_choice == "paper":
+   print("You lost!")
+  elif cpu_choice == "scissors":
+   print("You win!")
 
-elif user_choice == "scissors":
- if cpu_choice == "scissors":
-  print("It's a tie!")
- elif cpu_choice == "rock":
-  print("You lost!")
- elif cpu_choice == "paper":
-  print("You win!")
+ elif user_choice == "paper":
+  if cpu_choice == "paper":
+   print("It's a tie!")
+  elif cpu_choice == "scissors":
+   print("You lost!")
+  elif cpu_choice == "rock":
+   print("You win!")
+
+ elif user_choice == "scissors":
+  if cpu_choice == "scissors":
+   print("It's a tie!")
+  elif cpu_choice == "rock":
+   print("You lost!")
+  elif cpu_choice == "paper":
+   print("You win!")
