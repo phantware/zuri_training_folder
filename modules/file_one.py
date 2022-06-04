5a15d28dae9ecc61e6f9d2f7506979edfafaf7f0
phantware <phantmoney2011@gmail.com>
2022-06-04 14:31:29 +0100

    import checking

--- modules/file_one.py
+++ modules/file_one.py
@@ -1,3 +1,8 @@
 import file_two
 
 print("File one __name__ is set to: {}" .format(__name__))
+
+if __name__ == "__main__":
+ print("File one executed when ran directly")
+else:
+ print("File one executed when imported")
