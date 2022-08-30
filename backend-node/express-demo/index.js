f7a200af9cb87dad0ca13c46468117738f976642
phantware <phantmoney2011@gmail.com>
2022-08-30 07:55:48 +0100

    course object defined and course routers were setup

--- backend-node/express-demo/index.js
+++ backend-node/express-demo/index.js
@@ -1,22 +1,36 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
-app.get("/",(req,res)=>{
-    return res.status(200).json("hello world")
+const courses = [
+  { id: 1, name: 'course 1' },
+  { id: 2, name: 'course 2' },
+  { id: 3, name: 'course 3' },
+]
+
+app.get('/', (req, res) => {
+  return res.status(200).json('hello world')
 })
 
-app.get("/api/courses",(req,res)=>{
-    return res.status(200).json([1,2,3])
+app.get('/api/courses', (req, res) => {
+  return res.status(200).json(courses)
+})
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id))
+  if (!course) {
+    return res.status(404).json('The course with the given ID was not found')
+  }
+  return res.status(200).json(course)
 })
 
 // app.get("/api/posts/:year/:month",(req,res)=>{
 //     return res.status(200).json(req.params)
 // })
 
-app.get("/api/posts/:year/:month",(req,res)=>{
-    return res.status(200).json(req.query)
+app.get('/api/posts/:year/:month', (req, res) => {
+  return res.status(200).json(req.query)
 })
 
 const port = process.env.PORT || 3000
 
-app.listen(port,()=>console.log(`App running at port ${port}`))
+app.listen(port, () => console.log(`App running at port ${port}`))
