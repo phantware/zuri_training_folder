69e1a54551a959b0540ccf4490e63843e8e0b509
phantware <phantmoney2011@gmail.com>
2022-08-30 09:40:55 +0100

    Joi added for user validation

--- backend-node/express-demo/index.js
+++ backend-node/express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 app.use(express.json())
@@ -32,6 +33,16 @@ app.get('/api/posts/:year/:month', (req, res) => {
 })
 
 app.post('/api/courses', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  })
+  const validation = schema.validate(req.body)
+  //   console.log('validation', validation)
+
+  if (validation.error) {
+    return res.status(400).json(validation.error.details[0].message)
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,
