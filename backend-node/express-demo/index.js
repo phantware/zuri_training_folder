9202b9e567f613e9d2e65007a4fe7f036a6b8833
phantware <phantmoney2011@gmail.com>
2022-08-30 07:20:53 +0100

    port set at process.env

--- backend-node/express-demo/index.js
+++ backend-node/express-demo/index.js
@@ -9,4 +9,6 @@ app.get("/api/courses",(req,res)=>{
     return res.status(200).json([1,2,3])
 })
 
-app.listen(3000,()=>console.log("App running at port 3000"))
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>console.log(`App running at port ${port}`))
