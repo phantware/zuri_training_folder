638a74751f91cb52c4d80239e8e97116b1989e78
phantware <phantmoney2011@gmail.com>
2022-08-30 07:30:24 +0100

    requesting from server using params and query

--- backend-node/express-demo/index.js
+++ backend-node/express-demo/index.js
@@ -9,6 +9,14 @@ app.get("/api/courses",(req,res)=>{
     return res.status(200).json([1,2,3])
 })
 
+// app.get("/api/posts/:year/:month",(req,res)=>{
+//     return res.status(200).json(req.params)
+// })
+
+app.get("/api/posts/:year/:month",(req,res)=>{
+    return res.status(200).json(req.query)
+})
+
 const port = process.env.PORT || 3000
 
 app.listen(port,()=>console.log(`App running at port ${port}`))
