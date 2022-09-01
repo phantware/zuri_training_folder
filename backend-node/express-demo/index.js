25618cdc8a9dc1d778e84b7fe656cf8b2ce99964
phantware <phantmoney2011@gmail.com>
2022-09-01 06:39:35 +0100

    custom middleware explained

--- backend-node/express-demo/index.js
+++ backend-node/express-demo/index.js
@@ -1,7 +1,16 @@
 const Joi = require('joi')
+const log = require('./logger')
 const express = require('express')
 const app = express()
 app.use(express.json())
+
+app.use(log)
+
+app.use(function (req, res, next) {
+  console.log('Authenticating....')
+  next()
+})
+
 const courses = [
   { id: 1, name: 'course 1' },
   { id: 2, name: 'course 2' },
