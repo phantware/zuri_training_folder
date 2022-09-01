2ebc106a35b09cb29096c1b019a52ff9ed90b3b7
phantware <phantmoney2011@gmail.com>
2022-09-01 18:28:36 +0100

    Promise.all and Promise.race explained

--- backend-node/async-demo/promise-api.js
+++ backend-node/async-demo/promise-api.js
@@ -1,2 +1,23 @@
-const p = Promise.resolve({ id: 1 })
-p.then((result) => console.log(result))
+// const p = Promise.resolve({ id: 1 })
+// p.then((result) => console.log(result))
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('Async operation 1 ....')
+    // reject(new Error('Something went wrong!'))
+    resolve(1)
+  }, 2000)
+})
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('Async operation 2 ....')
+    resolve(2)
+  }, 2000)
+})
+
+// Promise.all([p1, p2])
+Promise.race([p1, p2])
+
+  .then((result) => console.log(result))
+  .catch((err) => console.log('Error', err.message))
