5e12899d4b9c6d0fd538f0aa8057c67bec9d8341
phantware <phantmoney2011@gmail.com>
2022-09-02 17:22:38 +0100

    express package added

---
+++ backend-node/mongo-demo/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+const app = express()
+dotenv.config()
+const PORT = process.env.PORT || 8800
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log('Connected to mongoDB.')
+  } catch (error) {
+    throw error
+  }
+}
+
+mongoose.connection.on('disconnected', () => {
+  console.log('mongoDB disconnected!')
+})
