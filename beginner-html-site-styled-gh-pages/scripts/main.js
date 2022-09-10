a3780e99dd774cd5639571a2a5fdd6563c7f97af
phantware <phantmoney2011@gmail.com>
2022-09-10 17:03:35 +0100

    image changing utilized using getAttribute and setAttribute

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,2 +1,12 @@
-const myHeading = document.querySelector('h1')
-myHeading.textContent = 'Hello World jamiu!'
+// const myHeading = document.querySelector('h1')
+// myHeading.textContent = 'Hello World jamiu!'
+
+const img = document.querySelector('img')
+img.onclick = () => {
+  const mySrc = img.getAttribute('src')
+  if (mySrc === 'images/firefox-icon.png') {
+    img.setAttribute('src', 'images/sun.png')
+  } else {
+    img.setAttribute('src', 'images/firefox-icon.png')
+  }
+}
