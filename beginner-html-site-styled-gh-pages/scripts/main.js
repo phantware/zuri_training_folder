ae91bc49ffc02c1caf8bd19648b79d2fbf6423ea
phantware <phantmoney2011@gmail.com>
2022-09-10 17:53:15 +0100

    user input check if not null the proceed to store the input in localstorage

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -17,8 +17,12 @@ let myHeading = document.querySelector('h1')
 
 function setUserName() {
   const myName = prompt('Please enter your name')
-  localStorage.setItem('name', myName)
-  myHeading.textContent = `Mozillar is cool, ${myName}`
+  if (!myName) {
+    setUserName()
+  } else {
+    localStorage.setItem('name', myName)
+    myHeading.textContent = `Mozillar is cool, ${myName}`
+  }
 }
 
 if (!localStorage.getItem('name')) {
