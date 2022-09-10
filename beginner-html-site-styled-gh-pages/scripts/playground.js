f6d97d9515b470ecb62825168e894ac2c86280db
phantware <phantmoney2011@gmail.com>
2022-09-11 00:23:51 +0100

    using join function explained

--- beginner-html-site-styled-gh-pages/scripts/playground.js
+++ beginner-html-site-styled-gh-pages/scripts/playground.js
@@ -1,4 +1,8 @@
-const myText = 'I am a string'
+// const myText = 'I am a string'
 
-const newString = myText.replace('string', 'sausage')
-console.log(newString)
+// const newString = myText.replace('string', 'sausage')
+// console.log(newString)
+
+const myArray = ['I', 'love', 'chocolate', 'frogs']
+const madeAString = myArray.join(' ')
+console.log(madeAString)
