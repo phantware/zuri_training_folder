bc87f26c4bf90caecdbb7a445d7628b364d4f8f9
phantware <phantmoney2011@gmail.com>
2022-09-10 17:41:55 +0100

    getting users input using prompt and saving it in localstorage and retrieving it when a button is clicked

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,12 +1,33 @@
 // const myHeading = document.querySelector('h1')
 // myHeading.textContent = 'Hello World jamiu!'
 
-const img = document.querySelector('img')
-img.onclick = () => {
-  const mySrc = img.getAttribute('src')
-  if (mySrc === 'images/firefox-icon.png') {
-    img.setAttribute('src', 'images/sun.png')
-  } else {
-    img.setAttribute('src', 'images/firefox-icon.png')
-  }
+// const img = document.querySelector('img')
+
+// img.onclick = () => {
+//   const mySrc = img.getAttribute('src')
+//   if (mySrc === 'images/firefox-icon.png') {
+//     img.setAttribute('src', 'images/sun.png')
+//   } else {
+//     img.setAttribute('src', 'images/firefox-icon.png')
+//   }
+// }
+
+let myButton = document.querySelector('button')
+let myHeading = document.querySelector('h1')
+
+function setUserName() {
+  const myName = prompt('Please enter your name')
+  localStorage.setItem('name', myName)
+  myHeading.textContent = `Mozillar is cool, ${myName}`
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName()
+} else {
+  const storeName = localStorage.getItem('name')
+  myHeading.textContent = `Mozilla is cool, ${storeName}`
+}
+
+myButton.onclick = () => {
+  setUserName()
 }
