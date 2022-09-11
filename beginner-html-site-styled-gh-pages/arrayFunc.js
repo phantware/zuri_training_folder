19f95ecb9d68f0b0cc796cee9936702980f87505
phantware <phantmoney2011@gmail.com>
2022-09-11 21:41:33 +0100

    filter function explained using array

--- beginner-html-site-styled-gh-pages/arrayFunc.js
+++ beginner-html-site-styled-gh-pages/arrayFunc.js
@@ -1,4 +1,5 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6]
-const result = numbers.map((item) => item * 2)
+// const result = numbers.map((item) => item * 2)
+const result = numbers.filter((item) => item % 2 === 0)
 
 console.log('result', result)
