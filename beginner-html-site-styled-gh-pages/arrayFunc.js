0896f66050d790f5a9d0ceac0efb70212c85a7f5
phantware <phantmoney2011@gmail.com>
2022-09-11 21:56:42 +0100

    reducer function explained

--- beginner-html-site-styled-gh-pages/arrayFunc.js
+++ beginner-html-site-styled-gh-pages/arrayFunc.js
@@ -1,5 +1,5 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6]
 // const result = numbers.map((item) => item * 2)
-const result = numbers.filter((item) => item % 2 === 0)
-
+// const result = numbers.filter((item) => item % 2 === 0)
+const result = numbers.reduce((prev, next) => prev + next, 0)
 console.log('result', result)
