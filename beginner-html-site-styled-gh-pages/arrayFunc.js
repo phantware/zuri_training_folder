41f4f7df81790a5735d6bc03b11cc1300050efae
phantware <phantmoney2011@gmail.com>
2022-09-11 22:01:34 +0100

    find function explained

--- beginner-html-site-styled-gh-pages/arrayFunc.js
+++ beginner-html-site-styled-gh-pages/arrayFunc.js
@@ -1,5 +1,6 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6]
 // const result = numbers.map((item) => item * 2)
 // const result = numbers.filter((item) => item % 2 === 0)
-const result = numbers.reduce((prev, next) => prev + next, 0)
+// const result = numbers.reduce((prev, next) => prev + next, 0)
+const result = numbers.find((item) => item > 5)
 console.log('result', result)
