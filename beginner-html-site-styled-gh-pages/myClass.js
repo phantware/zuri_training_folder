87e8ce7efccb64a3c8c3e880151455324347dd52
phantware <phantmoney2011@gmail.com>
2022-09-12 16:41:32 +0100

    how to write pentagon using class understood

---
+++ beginner-html-site-styled-gh-pages/myClass.js
@@ -0,0 +1,15 @@
+class Polygon {
+  constructor(...sides) {
+    this.sides = sides
+  }
+  // Method
+  *getSides() {
+    for (const side of this.sides) {
+      yield side
+    }
+  }
+}
+
+const pentagon = new Polygon(1, 2, 3, 4, 5)
+
+console.log([...pentagon.getSides()]) // [1,2,3,4,5]
