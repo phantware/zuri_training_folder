27231d8616350b42ef99c35cc0cb3f632f76f39c
phantware <phantmoney2011@gmail.com>
2022-09-12 11:19:50 +0100

    class exlplained

--- beginner-html-site-styled-gh-pages/arrayFunc.js
+++ beginner-html-site-styled-gh-pages/arrayFunc.js
@@ -1,6 +1,21 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6]
+// const numbers = [0, 1, 2, 3, 4, 5, 6]
 // const result = numbers.map((item) => item * 2)
 // const result = numbers.filter((item) => item % 2 === 0)
 // const result = numbers.reduce((prev, next) => prev + next, 0)
-const result = numbers.find((item) => item > 5)
-console.log('result', result)
+// const result = numbers.find((item) => item > 5)
+// console.log('result', result)
+
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+
+  stringSentence() {
+    return `Hello, my name is ${this.name} and I am ${this.age}`
+  }
+}
+
+const myPerson = new Person('Manu', 23)
+console.log(myPerson.age) // 23
+console.log(myPerson.stringSentence()) // "Hello, my name is Manu and I'm 23
