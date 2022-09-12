beeae382354271c0eed6f29c2d12cf05c3da6810
phantware <phantmoney2011@gmail.com>
2022-09-12 12:06:52 +0100

    sorting alphabets

--- beginner-html-site-styled-gh-pages/arrayFunc.js
+++ beginner-html-site-styled-gh-pages/arrayFunc.js
@@ -5,17 +5,26 @@
 // const result = numbers.find((item) => item > 5)
 // console.log('result', result)
 
-class Person {
-  constructor(name, age) {
-    this.name = name
-    this.age = age
-  }
+// class Person {
+//   constructor(name, age) {
+//     this.name = name
+//     this.age = age
+//   }
 
-  stringSentence() {
-    return `Hello, my name is ${this.name} and I am ${this.age}`
-  }
+//   stringSentence() {
+//     return `Hello, my name is ${this.name} and I am ${this.age}`
+//   }
+// }
+
+// const myPerson = new Person('Manu', 23)
+// console.log(myPerson.age) // 23
+// console.log(myPerson.stringSentence()) // "Hello, my name is Manu and I'm 23
+
+const months = ['March', 'Jan', 'Feb', 'Dec']
+
+// This function returns the length of the array for instance jan is less than feb, dec and feb are thesame length, it returns one of it an march comes last which is greater tha all
+function len_compare(a, b) {
+  return a.length - b.length
 }
-
-const myPerson = new Person('Manu', 23)
-console.log(myPerson.age) // 23
-console.log(myPerson.stringSentence()) // "Hello, my name is Manu and I'm 23
+months.sort(len_compare)
+console.log('months', months)
