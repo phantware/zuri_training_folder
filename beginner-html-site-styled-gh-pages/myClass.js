722c18bb292c7807915e68a17bf74d6289b1571f
phantware <phantmoney2011@gmail.com>
2022-09-12 17:04:02 +0100

    static class explained

--- beginner-html-site-styled-gh-pages/myClass.js
+++ beginner-html-site-styled-gh-pages/myClass.js
@@ -1,15 +1,40 @@
-class Polygon {
-  constructor(...sides) {
-    this.sides = sides
+// class Polygon {
+//   constructor(...sides) {
+//     this.sides = sides
+//   }
+//   // Method
+//   *getSides() {
+//     for (const side of this.sides) {
+//       yield side
+//     }
+//   }
+// }
+
+// const pentagon = new Polygon(1, 2, 3, 4, 5)
+
+// console.log([...pentagon.getSides()]) // [1,2,3,4,5]
+
+class Point {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
   }
-  // Method
-  *getSides() {
-    for (const side of this.sides) {
-      yield side
-    }
+
+  static displayName = 'Point'
+  static distance(a, b) {
+    const dx = a.x - b.x
+    const dy = a.y - b.y
+
+    return Math.hypot(dx, dy)
   }
 }
 
-const pentagon = new Polygon(1, 2, 3, 4, 5)
+const p1 = new Point(5, 5)
+const p2 = new Point(10, 10)
+p1.displayName // undefined
+p1.distance // undefined
+p2.displayName // undefined
+p2.distance // undefined
 
-console.log([...pentagon.getSides()]) // [1,2,3,4,5]
+console.log(Point.displayName) // "Point"
+console.log(Point.distance(p1, p2)) // 7.0710678118654755
