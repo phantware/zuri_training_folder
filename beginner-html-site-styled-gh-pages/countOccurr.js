ecf4349c6bcf9e48da1e1b2bb52d4e2aa2aebf62
phantware <phantmoney2011@gmail.com>
2022-09-15 15:03:13 +0100

    countOccurence iteration understood

--- beginner-html-site-styled-gh-pages/countOccurr.js
+++ beginner-html-site-styled-gh-pages/countOccurr.js
@@ -12,18 +12,33 @@
 
 // console.log(count)
 
-const arr = ['a', 'b', 'a', 'a', 'c', 'c']
+// const arr = ['a', 'b', 'a', 'a', 'c', 'c']
 
-const count = {}
+// const count = {}
 
-for (let index = 0; index < arr.length; index++) {
-  const element = arr[index]
+// for (let index = 0; index < arr.length; index++) {
+//   const element = arr[index]
 
-  if (count[element]) {
-    count[element] += 1
-  } else {
-    count[element] = 1
-  }
+//   if (count[element]) {
+//     count[element] += 1
+//   } else {
+//     count[element] = 1
+//   }
+// }
+
+// console.log(count) // 👉️ {a: 3, b: 1, c: 2}
+
+const iterable = new Map([
+  ['a', 1],
+  ['b', 2],
+  ['c', 3],
+])
+
+for (const entry of iterable) {
+  console.log('entry', entry)
 }
+console.log('iterable', iterable)
 
-console.log(count) // 👉️ {a: 3, b: 1, c: 2}
+for (const [key, value] of iterable) {
+  console.log('value', value)
+}
