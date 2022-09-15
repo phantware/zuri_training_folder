0b724b2b9dc388c0f1b3fd6a0b4c15257253e84d
phantware <phantmoney2011@gmail.com>
2022-09-15 13:20:33 +0100

    count occurence understood

---
+++ beginner-html-site-styled-gh-pages/countOccurr.js
@@ -0,0 +1,13 @@
+const arr = ['a', 'b', 'a', 'a', 'b', 'c', 'c']
+
+const count = {}
+
+for (const element of arr) {
+  if (count[element]) {
+    count[element] += 1
+  } else {
+    count[element] = 1
+  }
+}
+
+console.log(count)
