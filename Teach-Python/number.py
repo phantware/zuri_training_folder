05060bda5953047084612e240f8e5f0100f5395a
phantware <phantmoney2011@gmail.com>
2022-09-26 23:29:59 +0100

    number.py

---
+++ Teach-Python/number.py
@@ -0,0 +1,40 @@
+'''
+This data type supports only numerical values like 1, 31.4, -1000, 0.000023, 88888888.
+
+Python supports 3 different numerical types.
+
+int - for integer values like 1, 100, 2255, -999999, 0, 12345678.
+float - for floating-point values like 2.3, 3.14, 2.71, -11.0.
+complex - for complex numbers like 3+2j, -2+2.3j, 10j, 4.5+3.14j.
+
+'''
+
+# Integers 
+
+i = 100
+
+# Floats
+
+f = 12.3
+
+
+# Complex number
+# As you may know complex number consists of two parts real and imaginary, and is denoted by j. You can define complex number like this:
+
+x = 2 + 3j # where 2 is the real part and 3 is imaginary
+
+# Float Division (/) : The /  operator divides and return result as floating point number means it will always return fractional part. For e.g
+
+a = 3/2 # 1.5
+
+# Integer Division (//) : The // perform integer division i.e it will truncate the decimal part of the answer and return only integer.
+
+b = 3//2 # 1
+
+# Exponentiation Operator (**) : This operator helps to compute ab (a raise to the power of b). Let's take an example:
+
+c = 2 ** 3 # is same as 2 * 2 * 2 = 8
+
+# Remainder operator (%) : The % operator also known as remainder or modulus operator. This operator return remainder after division. For e.g:
+
+d = 7 % 2 # 1
