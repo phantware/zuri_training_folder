cae8399ab62353941d8f5f010089207d5f64c3e0
phantware <phantmoney2011@gmail.com>
2022-09-26 22:49:37 +0100

    running python explained

---
+++ Teach-Python/running-python-program.py
@@ -0,0 +1,5 @@
+# Running python programs
+
+# You can run python programs in two ways, first by typing commands directly in python shell or run program stored in a file. But most of the time you want to run programs stored in a file.
+
+print("Hello World!!")
