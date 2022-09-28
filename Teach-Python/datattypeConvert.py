f0835399a602105a13ccd27ab64ef55cc624018e
phantware <phantmoney2011@gmail.com>
2022-09-28 07:48:42 +0100

    datatype conversion in python explained

---
+++ Teach-Python/datattypeConvert.py
@@ -0,0 +1,35 @@
+# Converting int to float
+# To convert int to float you can use the float() function.
+
+i = 10
+float(i) #  10.0
+
+# Converting float to int
+# To convert float to int you need to use int() function.
+
+f = 14.66
+int(f)  #  14
+
+# Converting string to int
+# To convert string to int, use the int() function.
+
+s = "123"
+int(s)  #  123
+
+# Converting number to string
+# To convert a number to string use the str() function.
+
+i = 100
+str(i) #  "100"
+
+f = 1.3
+str(f)  #  '1.3'
+
+'''
+Rounding numbers
+Rounding numbers is done via the round() function.
+
+Syntax: round(number[, ndigits])
+'''
+i = 23.97312
+round(i, 2) #  23.97
