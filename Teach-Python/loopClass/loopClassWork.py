5669b3c74a86920c38d34cf61feae72867e907a7
phantware <phantmoney2011@gmail.com>
2022-10-05 01:21:38 +0100

    looping and checking for odd numbers

---
+++ Teach-Python/loopClass/loopClassWork.py
@@ -0,0 +1,9 @@
+# Use for loop through the list from 1 to 21
+# Use Modulus to check that the result is NOT  EQUAL to 0
+# Print the odds
+
+# Solution
+
+for i in range(1,21):
+ if((i % 2) != 0):
+  print("i = ", i)
