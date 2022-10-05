31e5746fa4ef905f2788a1912cceb4ce3dbae80e
phantware <phantmoney2011@gmail.com>
2022-10-05 01:26:25 +0100

    convert float to 2 decimal places

--- Teach-Python/loopClass/loopClassWork.py
+++ Teach-Python/loopClass/loopClassWork.py
@@ -4,6 +4,16 @@
 
 # Solution
 
-for i in range(1,21):
- if((i % 2) != 0):
-  print("i = ", i)
+# for i in range(1,21):
+#  if((i % 2) != 0):
+#   print("i = ", i)
+
+# Write a program that takes in a float number
+# Round the float number to 2 decimal places
+# Print out the result
+
+# Solution
+
+your_numb = input("Enter a float: ")
+your_numb = float(your_numb)
+print("Round to 2 decimal : {:.2f}".format(your_numb))
