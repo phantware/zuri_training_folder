b9e77b6e9e436d313454bd5382507f5cf48d1125
phantware <phantmoney2011@gmail.com>
2022-10-09 17:59:29 +0100

    string checks

---
+++ Teach-Python/stringFunc/stringCheck.py
@@ -0,0 +1,14 @@
+letter_z = "z"
+num_3 = "3"
+a_space = " "
+
+print("Is z a letter or number? ", letter_z.isalnum())
+print("Is z a letter? ", letter_z.isalpha())
+print("Is 3 a number? ", letter_z.isdigit())
+print("Is z a lowercase? ", letter_z.islower())
+print("Is z a uppercase? ", letter_z.isupper())
+print("Is Space a space? ", letter_z.isspace())
+
+
+
+
