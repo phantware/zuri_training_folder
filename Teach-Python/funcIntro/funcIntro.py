bf71855c30a5612f82d305fcbe5d7fb159534d8d
phantware <phantmoney2011@gmail.com>
2022-10-11 00:30:13 +0100

    local variable

--- Teach-Python/funcIntro/funcIntro.py
+++ Teach-Python/funcIntro/funcIntro.py
@@ -4,7 +4,15 @@ def add_number(num1, num2):
   return num1 + num2
 print("5 + 4 = ", add_number(5,4))
 
+# Local Variable: This only work inside a function
+# Example:
+def assing_name():
+  name = "Lala"
 
+assing_name()
+print(name)   # This will through an error because name is a local variable
+
+# Global Variable: This works both inside and outside a function
 
 
 # defining a function in python
