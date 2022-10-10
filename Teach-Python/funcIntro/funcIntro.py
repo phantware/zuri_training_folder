befd65b215156e61ec7560548ba97be22cfdfa35
phantware <phantmoney2011@gmail.com>
2022-10-11 00:26:09 +0100

    funcIntro.py

--- Teach-Python/funcIntro/funcIntro.py
+++ Teach-Python/funcIntro/funcIntro.py
@@ -1,11 +1,20 @@
-# defining a function in python
-num_3 = "3.142"
+# Functions allow us to reuse code, we don't have to retype code over and over again
 
-def isFloat(str_val):
- try:
-  float(str_val)
-  return True
- except ValueError:
-  return False
-print("Is PI a Float? ", isFloat(num_3))
+def add_number(num1, num2):
+  return num1 + num2
+print("5 + 4 = ", add_number(5,4))
+
+
+
+
+# defining a function in python
+# num_3 = "3.142"
+
+# def isFloat(str_val):
+#  try:
+#   float(str_val)
+#   return True
+#  except ValueError:
+#   return False
+# print("Is PI a Float? ", isFloat(num_3))
 
