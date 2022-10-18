5debe9915c5dcc0d02f9c9776d14151b51a54d45
phantware <phantmoney2011@gmail.com>
2022-10-18 02:29:33 +0100

    dictionay classwork 1

--- Teach-Python/dictClass/dictClassWork.py
+++ Teach-Python/dictClass/dictClassWork.py
@@ -1,3 +1,7 @@
+
+# Using your knowledge of how dictionary works, create a program to get an employees information from the user input
+# Print out the result in a dictionary format
+
 employees = []
 fName, lName = input("Enter Employee Name: ").split()
 employees.append({"fName":fName, "lName": lName})
