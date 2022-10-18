53ff8aef9c71a6dc562e15b8f16b0481d9f358b6
phantware <phantmoney2011@gmail.com>
2022-10-18 04:00:47 +0100

    fibo series created

--- Teach-Python/python basics/recursiveFunc.py
+++ Teach-Python/python basics/recursiveFunc.py
@@ -2,13 +2,13 @@
 # Let say we want to calculate a factorials of 3! which is 3 * 2 * 1
 # How will we use recussion to solve this problems?
 
-def factorial(num):
- if num <= 1:
-  return 1
- else:
-  result = num * factorial(num - 1)
-  return result
-print("4! = ", factorial(4)) 
+# def factorial(num):
+#  if num <= 1:
+#    return 1
+#  else:
+#    result = num * factorial(num - 1)
+#    return result
+# print("4! = ", factorial(4)) 
 
 '''
 DRY running the program
@@ -18,3 +18,28 @@ DRY running the program
 3st iteration: result  = 2 * factorial (2 - 1)  2 * 1
 
 '''
+
+# Calculate Fibonacci numbers
+# For Fibonacci series, we have 1, 1, to calculate the next value we add the previous numbers together which = 2, it goes like that, 1, 1, 2, 3, 5, 8, 13
+# To calculate this, we use the formula fn = fn - 1 + fn - 2
+# Where F0 = 0, and F1 = 1
+# print(fib(3))
+
+'''
+DRY running the program
+
+1st iteration: result  = fib(2) + fib(1) :  2 + 1 = 3
+2st iteration: result  = (fib(1) + fib(0 )) + fib(0) : 1 + 0  = 1
+'''
+
+def fibo(num):
+ if num == 0:
+   return 0
+ elif num == 1:
+  return 1
+ else:
+   result = fibo(num - 1) + (num - 2)
+   return result
+print(fibo(3))
+print(fibo(4))
+print(fibo(5))
