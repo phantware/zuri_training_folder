f839f07730ad552a73809c54d57b7a7f4bad4b57
phantware <phantmoney2011@gmail.com>
2022-10-26 13:48:55 +0100

    Palindrome tasks

---
+++ HTML_SEMANTICS/pali.js
@@ -0,0 +1,23 @@
+// program to check if the string is palindrome or not
+
+function checkPalindrome(string) {
+  // convert string to an array
+  const arrayValues = string.split('') // ["h", "e", "l", "l", "o"]
+
+  // reverse the array values
+  const reverseArrayValues = arrayValues.reverse() // ["o", "l", "l", "e", "h"]
+
+  // convert array to string
+  const reverseString = reverseArrayValues.join('') // "olleh"
+
+  if (string == reverseString) {
+    console.log('It is a palindrome')
+  } else {
+    console.log('It is not a palindrome')
+  }
+}
+
+//take input
+const string = prompt('Enter a string: ')
+
+checkPalindrome(string)
